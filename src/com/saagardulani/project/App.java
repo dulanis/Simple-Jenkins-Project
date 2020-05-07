0b3ea6b423ebd86ed0b6c4d6fdca7042d39c2f69
dulanis <saagardulani96@gmail.com>
2020-05-07 11:10:46 -0400

    Added a second line to be printed to the console

--- src/com/saagardulani/project/App.java
+++ src/com/saagardulani/project/App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my simple Jenkins project.");
+		
+		System.out.println("Here is an additional line so as to make another commit");
 
 	}
 
